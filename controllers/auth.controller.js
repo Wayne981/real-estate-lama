85e936ad40d48edefcea4b58df478992de07e780
Chiranthan M <chiranthanmahesh007@gmail.com>
2024-12-22 15:20:21 +0530

    initial commit

---
+++ controllers/auth.controller.js
@@ -0,0 +1,20 @@
+import bcrypt from "bcrypt";
+
+export const register = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  // HASH THE PASSWORD
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  console.log(hashedPassword);
+
+  // CREATE A NEW USER AND SAVE TO DB
+};
+
+export const login = (req, res) => {
+  // db operations
+};
+
+export const logout = (req, res) => {
+  // db operations
+};
